2c021e27cf1ee6da94045d9e205a92007638672f
Valeriia Ladyhina <v.ladyhina@grug.nl>
2021-10-04 18:12:56 +0200

    fixed some spellings

--- units_converter.py
+++ units_converter.py
@@ -3,7 +3,7 @@ def main():
     Welcome to converter of units. 
     By using it you can convert mass and density.
 
-    Short guide line:
+    Short guideline:
 
     1) To start with print needed type of measurements:
         * mass
@@ -20,14 +20,14 @@ def main():
     while True:
         command = input()
         if command not in commands and command not in exiting_messages:
-            print('Please check spelling of the command and try again')
+            print('Please check spelling of the command and try again.')
         else:
             if command == 'mass':
                 mass_converter()
             elif command == 'density':
-                print('We are very sorry. Currently this function is in development, try our mass converter')
+                print('We are very sorry. Currently this function is in development, try our mass converter.')
             elif command in exiting_messages:
-                print('Thank you for using our tool')
+                print('Thank you for using our tool.')
                 break
 
 
@@ -41,36 +41,37 @@ def mass_converter():
           '\n'
           '     * British/American units: gr <-> dr <->oz <-\n '
           '     -> lb <-> short ton <-> long ton <-> st\n')
-    print('Please write unit type to be converted')
+    print('Please write unit type to be converted:')
     unit_type_from = input()
     while unit_type_from not in SI_mass.keys() and unit_type_from not in British_American_mass.keys():
-        print('Please check your spelling and try again')
+        print('Please check your spelling and try again:')
         unit_type_from = input()
-    print('Please write value of interest')
+    print('Please write value of interest:')
     value = input()
     while not value.isdigit():
-        print('Input has to be a number. Try again')
+        print('Input has to be a number. Try again:')
         value = input()
     value = int(value)
-    print('Please fill in unit type to which would like to convert')
+    print('Please fill in unit type to which would like to convert:')
     unit_type_to = input()
     while unit_type_to not in SI_mass.keys() and unit_type_to not in British_American_mass.keys():
-        print('Please check your spelling and try again')
+        print('Please check your spelling and try again:')
         unit_type_to = input()
     if unit_type_from in SI_mass.keys():
         g_value = value * SI_mass[unit_type_from]
     else:
         g_value = value * British_American_mass[unit_type_from]
-    if unit_type_to != 'g':
-        if unit_type_to in SI_mass.keys():
-            result_value = g_value / SI_mass[unit_type_to]
-        else:
-            result_value = g_value / British_American_mass[unit_type_to]
+    if unit_type_to in SI_mass.keys():
+        result_value = g_value / SI_mass[unit_type_to]
+    else:
+        result_value = g_value / British_American_mass[unit_type_to]
+
     print(value, unit_type_from, '=', result_value, unit_type_to)
     print('Try other values to convert. '
-          'Print needed type of measurements. Choose:'
-          '     * mass'
-          '     * density')
+          'Print needed type of measurements. Choose:\n'
+          '     * mass\n'
+          '\n'
+          '     * density\n')
 
 
 if __name__ == "__main__":
