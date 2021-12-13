da02d6b931d82258ead2a3a37f26139293e53851
V.Ladyhina <valeriia.ladyhina@gmail.com>
2021-12-13 16:29:07 +0100

    mkdir

--- HW_sys_os/sort.py
+++ HW_sys_os/sort.py
@@ -5,12 +5,12 @@ import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', metavar='FILE', nargs='*', help='file to be sorted', default='-')
+    parser.add_argument('file', nargs='?', help='file to be sorted', default='-')
     args = parser.parse_args()
     file = args.file
+    print(file)
     if file == '-':
         file = sys.stdin
-        print(file)
     else:
         file = open(args.file).readlines()
     print(*sorted(file))
