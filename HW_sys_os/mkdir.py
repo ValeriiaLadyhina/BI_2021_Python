902aa3bfdb6bafc5050d0f461d5448d5df49852c
V.Ladyhina <71066938+ValeriiaLadyhina@users.noreply.github.com>
2021-12-13 18:12:56 +0100

    Update mkdir.py

--- HW_sys_os/mkdir.py
+++ HW_sys_os/mkdir.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p',  '--parents',  action='store_true',
                         help='no error if existing, make parent directories as needed')
-    parser.add_argument('dir_name', help='name of directory you want to check', nargs='?', const='.')
+    parser.add_argument('dir_name', help='name of directory you want to create', nargs='?', const='.')
     args = parser.parse_args()
     dir_name = args.dir_name
     p = args.parents
