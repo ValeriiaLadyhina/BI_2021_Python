8b1510b876f906eea2ebd116e7aedf78da6a93e6
V.Ladyhina <valeriia.ladyhina@gmail.com>
2021-12-13 17:49:34 +0100

    not fully done

---
+++ HW_sys_os/tail.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', nargs='?',
+                        help='if tou want to show last 10 lines of few documents', default='-')
+    parser.add_argument('file',  help='file/files to process to show', nargs='+', default='-')
+    args = parser.parse_args()
+    n = args.n
+    if n == '-':
+        file = args.file
+        file = open(file).readlines()
+        print(file)
+        if len(file) < 10:
+            for line in range(len(file)):
+                print(file[line])
+        else:
+            for line in range(-10, -1):
+                print(file[line])
+    else:
+        for i in range(0, len(args.file)):
+            file = args.file[i]
+            file = open(file).readlines()
+            if len(file) < 10:
+                for line in range(len(file)):
+                    print(file[line])
+            else:
+                for line in range(-10, -1):
+                    print(file[line])
